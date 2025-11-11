d17555de7ec344214d5160c4f2c4a660215c8269
Shruti <dev.shruti27@gmail.com>
2025-11-11 15:23:18 +0100

    set auto-focus to false

--- src/components/StartQuiz.tsx
+++ src/components/StartQuiz.tsx
@@ -18,15 +18,18 @@ export default function StartQuiz({
 
   return (
     <dialog
-      className="w-full h-fit max-w-[50rem] mx-auto my-[16rem] px-[2rem] py-[8rem] bg-(--bg-lilac-200)/90 shadow-[1px_1px_8px_4px_rgba(12,5,32,0.6)] rounded-[0.5rem] font-bold"
+      className="w-full h-fit max-w-200 mx-auto my-64 px-8 py-32 bg-(--bg-lilac-200)/90 shadow-[1px_1px_8px_4px_rgba(12,5,32,0.6)] rounded-lg font-bold"
       ref={dialogRef}
     >
-      <button
-        className="max-w-[50%] w-full inline-block py-[1rem] px-[2rem] text-sm font-[Roboto-Condensed] bg-(--color-purple-400)/90 text-(--text-light-lilac-100) drop-shadow-[0_5px_5px_rgba(0,0,0,0.6)] hover:bg-(--bg-dusty-yellow) focus:bg-(--bg-dusty-yellow) hover:text-(--text-purple-800) focus:text-(--text-purple-800)  uppercase rounded-[1.5rem] cursor-pointer transition-all duration-400 ease-in-out"
-        onClick={onStart}
-      >
-        Start Quiz
-      </button>
+      <form method="dialog">
+        <button
+          autoFocus={false}
+          className="max-w-[50%] w-full inline-block py-4 px-8 text-sm font-[Roboto-Condensed] bg-purple-400/90 text-(--text-light-lilac-100) drop-shadow-[0_5px_5px_rgba(0,0,0,0.6)] hover:bg-(--bg-dusty-yellow) focus:bg-(--bg-dusty-yellow) hover:text-(--text-purple-800) focus:text-(--text-purple-800)  uppercase rounded-3xl cursor-pointer transition-all duration-400 ease-in-out"
+          onClick={onStart}
+        >
+          Start Quiz
+        </button>
+      </form>
     </dialog>
   );
 }
